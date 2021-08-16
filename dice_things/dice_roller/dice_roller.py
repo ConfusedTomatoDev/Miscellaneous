bf323f41f06751de75cd141627742f4e0fe18568
ConfusedTomatoDev <67872038+ConfusedTomatoDev@users.noreply.github.com>
2021-08-16 10:48:57 -0700

    Create dice_roller.py
    Rolls a x sided dice x times for x turns.

---
+++ dice_things/dice_roller/dice_roller.py
@@ -0,0 +1,29 @@
+# Basic Dice rolling code, feel free to use for your own needs.
+# Rolls a x sided dice x times for x turns.
+#
+from random import randint
+
+# Pick the number of dice sides
+dice_sides = 20
+
+# Pick the number of rolls per turn
+dice_rolls = 20
+
+# Pick the number of turns
+turns = 5
+
+# Counters
+count_turns = 1
+
+# Functions
+def roll_dice():
+    roll_count = 0
+    while roll_count < dice_rolls:
+        print(randint(1,dice_sides))
+        roll_count += 1
+
+# Main routine
+while count_turns != turns+1:
+    print ("\nRoll", count_turns,"\n")
+    roll_dice()
+    count_turns += 1
