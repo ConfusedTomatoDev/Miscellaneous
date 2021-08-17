8e0f63bc06d98aa854b441fcb35e105252602b39
ConfusedTomatoDev <67872038+ConfusedTomatoDev@users.noreply.github.com>
2021-08-17 15:01:26 -0700

    Update dice_roller.py
    Big update to include the following information:
    average, min, lower quaderant, median, upper quaderant, max, mean, stdv, frequency table

--- dice_things/dice_roller/dice_roller.py
+++ dice_things/dice_roller/dice_roller.py
@@ -1,9 +1,17 @@
 # Basic Dice rolling code, feel free to use for your own needs.
 # Rolls a x sided dice x times for x turns.
+# and returns the following: average, min, lower quaderant, median, upper quaderant, max, mean, stdv, frequency table
 # Developer: ConfusedTomatoDev
 # Created 08/16/2021
 #
+# Sites that helped with how to formulate and present the numbers.
+# https://www.mathsisfun.com/data/quartiles.html
+# https://www.hackmath.net/en/calculator/quartile-q1-q3
+#
+import statistics
+import numpy as npmath
 from random import randint
+from statistics import mean
 
 # Pick the number of dice sides
 dice_sides = 20
@@ -12,32 +20,84 @@ dice_sides = 20
 dice_rolls = 20
 
 # Pick the number of turns
-turns = 5
+turns = 20
 
 # Counters and lists
 count_turns = 1
+roll_list = []
 average_list = []
+
+
+# Classes/Objects
+# None yet
 
 # Functions
 def roll_dice():
-    roll_count = 0
-    print ("Rolling...")
-    while roll_count < dice_rolls:
-        roll = (randint(1,dice_sides))
-        # Uncomment line below if you need vertical list of rolls
-        #print (roll)
-        roll_count += 1
-        average_list.append(roll)
-        
+	roll_count = 0
+	print("Rolling...")
+	while roll_count < dice_rolls:
+		roll = (randint(1, dice_sides))
+		# Uncomment line below if you need vertical list of rolls
+		#print (roll)
+		roll_count += 1
+		roll_list.append(roll)
+
+
+def CountFrequency(average_list):
+	# Creating an empty dictionary
+	freq = {}
+	for item in average_list:
+		if (item in freq):
+			freq[item] += 1
+		else:
+			freq[item] = 1
+	for key, value in freq.items():
+		#print (key ,"     ", value)
+		print(f'{key:3d}    {value:3d}')
+
+
 # Main routine
-while count_turns != turns+1:
-    print("Turn:", count_turns)
-    roll_dice()
-    print("Rolls: " + ", ".join(map(str, average_list)))
-    #print ("Averaging")
-    average_total = sum(average_list) / len(average_list)
-    #print ("Done averaging")
-    print("Average:", round(average_total))
-    print("============================\n")
-    average_list.clear()
-    count_turns += 1
+while count_turns != turns + 1:
+	print("Turn number:", count_turns)
+	roll_dice()
+	print("Roll: " + ", ".join(map(str, roll_list)))
+	roll_list.sort()
+	print("Sorted Roll: " + ", ".join(map(str, roll_list)))
+	#print("Done Rolling, calculating...")
+	#print ("Averaging...")
+	average_ans = sum(roll_list) / len(roll_list)
+	average_list.append(round(average_ans))
+	#print ("Done averaging...")
+	print("Average of roll:", round(average_ans))
+	print("\n")
+	print("Average each turn: " + ", ".join(map(str, average_list)))
+	average_list.sort()
+	print("Sorted average of each turn: " + ", ".join(map(str, average_list)))
+	print("\n")
+	print("Minimum value of average turns:", min(average_list))
+	print("Rounded minimum value of average turns:", round(min(average_list)))
+	print("Q1 quantile of average turns: ", npmath.quantile(average_list, .25))
+	print("Rounded Q1 quantile of average turns: ", round(npmath.quantile(average_list, .25)))
+	print("Q2 quantile of average turns: ", npmath.quantile(average_list, .50))
+	print("Rounded Q2 quantile of average turns: ", round(npmath.quantile(average_list, .50)))
+	print("Q3 quantile of average turns: ", npmath.quantile(average_list, .75))
+	print("Rounded Q3 quantile of average turns: ", round(npmath.quantile(average_list, .75)))
+	print("Maximum value of average turns:", max(average_list))
+	print("Rounded maximum value of average turns:", round(max(average_list)))
+	print("\n")
+	print("Mean of turns:", mean(average_list))
+	print("Rounded mean of turns:", round(mean(average_list)))
+	print("Median of turns:", npmath.median(npmath.array(average_list)))
+	print("Rounded median of turns:", round(npmath.median(npmath.array(average_list))))
+	res = statistics.pstdev(average_list)
+	print("Standard deviation of turns: " + str(res))
+	print("Rounded standard deviation turns: ", round(res))
+	print("\n")
+	print("Data frequency of average from turns:")
+	print("-------------------------------------")
+	print("Data    Freq")
+	CountFrequency(average_list)
+	print("=====================================\n")
+	print("Results are based on the following: Number of dice sides: ", dice_sides, "Number of dice rolls: ", dice_rolls, "Number of turns: ", turns, "\n")
+	roll_list.clear()
+	count_turns += 1
