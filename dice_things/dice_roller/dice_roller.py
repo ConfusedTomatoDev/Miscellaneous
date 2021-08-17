78b9b0c89a6d5cd6b00ff427ede6cdf72438a7f8
ConfusedTomatoDev <67872038+ConfusedTomatoDev@users.noreply.github.com>
2021-08-16 19:33:45 -0700

    Update dice_roller.py
    Added cleaner list print and average of all rolls.

--- dice_things/dice_roller/dice_roller.py
+++ dice_things/dice_roller/dice_roller.py
@@ -1,7 +1,7 @@
 # Basic Dice rolling code, feel free to use for your own needs.
 # Rolls a x sided dice x times for x turns.
 # Developer: ConfusedTomatoDev
-# Created 08/16/2021 CTD
+# Created 08/16/2021
 #
 from random import randint
 
@@ -14,19 +14,30 @@ dice_rolls = 20
 # Pick the number of turns
 turns = 5
 
-# Counters
+# Counters and lists
 count_turns = 1
+average_list = [0]
 
 # Functions
 def roll_dice():
     roll_count = 0
+    print ("Rolling")
     while roll_count < dice_rolls:
-        print(randint(1,dice_sides))
+        roll = (randint(1,dice_sides))
+        print (roll)
         roll_count += 1
-
+        average_list.append(roll)
+        
 # Main routine
 while count_turns != turns+1:
-    print ("\nRoll", count_turns,"\n")
+    #print("List", average_list)
+    print("Rolls " + ", ".join(map(str, average_list)))
+    #print ("Averaging")
+    average_total = sum(average_list) / len(average_list)
+    #print ("Done averaging")
+    print("Average", round(average_total))
+    print("============================\n")
+    print("Turn", count_turns)
+    average_list.clear()
     roll_dice()
     count_turns += 1
-   
