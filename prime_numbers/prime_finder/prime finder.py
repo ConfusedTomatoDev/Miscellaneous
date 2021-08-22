1ce5efd075870650649aa12a2df3407583a3a280
ConfusedTomatoDev <confusedtomatoguy@gmail.com>
2021-08-22 15:28:57 -0700

    var rename

--- prime_numbers/prime_finder/prime finder.py
+++ prime_numbers/prime_finder/prime finder.py
@@ -9,28 +9,28 @@
 def print_primes_till_N(N):
 
 	# Define the variables
-	i, ct, flag = 0, 0, 0;
+	prime_check, ct, flag = 0, 0, 0;
 
 	# Print display message
 	print("Found these prime numbers between 1 and",N , "are:");
 
 	# Loop for each number from 1 to N
-	for i in range(1, N + 1, 1):
+	for prime_check in range(1, N + 1, 1):
 
 		# Skip 0 and 1 as they are not prime or composite numbers
-		if (i == 1 or i == 0):
+		if (prime_check == 1 or prime_check == 0):
 			continue;
 
 		# Mark variable and check see if it is prime or not
 		flag = 1;
 
-		for ct in range(2, ((i // 2) + 1), 1):
-			if (i % ct == 0):
+		for ct in range(2, ((prime_check // 2) + 1), 1):
+			if (prime_check % ct == 0):
 				flag = 0;
 				break;
 		# flag = 1 means  it is prime and flag = 0 means it is not prime
 		if (flag == 1):
-			print(i, end = " ");
+			print(prime_check, end = " ");
 
 # Main routine
 N = 1000;
