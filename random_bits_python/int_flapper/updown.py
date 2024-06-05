9cfdb814160f456a72fd206a5ce2067c1184ebca
ConfusedTomatoDev <67872038+ConfusedTomatoDev@users.noreply.github.com>
2024-06-05 11:28:19 -0700

    Update updown.py
    A tool used to flap Linux host interfaces between a min/max interval.  Useful for fuzzing connectivity outages.

--- random_bits_python/int_flapper/updown.py
+++ random_bits_python/int_flapper/updown.py
@@ -1,1 +1,63 @@
+#######################################################
+#    Developer: ConfusedTomatoDev
+#    Created 06/03/2024
+#
+#    updown.py
+#    Use this script from root to flap
+#    interfaces on a linux host at random intervals
+#######################################################
 
+import os
+import random
+import time
+import signal
+import sys
+from datetime import datetime
+
+# Define interface
+interface = "eth1"
+
+# Catch the time
+curtime = datetime.now().strftime("%Y-%m-%d " "%H:%M:%S")
+
+def timestamp():
+    return datetime.now().strftime("%H:%M:%S")
+
+# Define flap interval and interval to sleap between flaps.
+minint = 1
+maxint = 15
+pauseint = 5
+
+# Prepare for the unexpected
+def cleanup(signum, frame):
+    print(f"Caught signal {signum}, ifup {interface}")
+    os.system(f"ifup {interface}")
+    sys.exit(0)
+
+# SIGHUP
+signal.signal(signal.SIGHUP, cleanup)
+# SIGINT
+signal.signal(signal.SIGINT, cleanup)
+# SIGTERM
+signal.signal(signal.SIGTERM, cleanup)
+
+# Start the flapping
+try:
+    while True:
+        print(f"#### {curtime} ####")
+        print(f"{timestamp()} - Interval Start")
+        os.system(f"ifdown {interface}")
+        interval = random.randint(minint, maxint)
+        print(f"Start selected downtime {interval}")
+        time.sleep(interval)
+        os.system(f"ifup {interface}")
+        print(f"{timestamp()} - End selected downtime")
+        time.sleep(pauseint)
+        print(f"Up after {pauseint}")
+        print(f"{timestamp()} Wakeup\n\n")
+
+# Handle the unexpected
+except Exception as e:
+    print(f"Script ended unexpectedly: {e}")
+    os.system(f"ifup {interface}")
+    print("Ended Cycle\n")
